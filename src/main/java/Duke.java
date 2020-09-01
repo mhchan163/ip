f16fd65c97451f56a2fdc62f03ba59dcb0e008d1
mhchan163 <mhchan163@gmail.com>
2020-09-01 12:10:53 +0800

    Added Duke Level 2.Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,13 +13,24 @@ public class Duke {
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
         Scanner in = new Scanner(System.in);
+        String[] list = new String[100];
         String line;
-        line = in.nextLine();
-        while(!line.equals("bye")){
-            System.out.println("____________________________________________________________");
-            System.out.println(line);
-            System.out.println("____________________________________________________________");
+        for(int i=0;i<list.length;i++){
             line = in.nextLine();
+            if(line.equals("bye")){
+                break;
+            } else if(line.equals("list")){
+                System.out.println("____________________________________________________________");
+                for(int j=0;j<i;j++) {
+                    System.out.println(j + 1 + "." + list[j]);
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                list[i] = line;
+                System.out.println("____________________________________________________________");
+                System.out.println("Added: "+ line);
+                System.out.println("____________________________________________________________");
+            }
         }
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
